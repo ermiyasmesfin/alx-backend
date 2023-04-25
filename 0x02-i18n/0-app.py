687cf56c10b9737821521929dc1f3ccf631e3ce4
Ermiyas <98635649+ermiyasmesfin@users.noreply.github.com>
2023-04-25 11:25:21 +0300

    last hope

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python3
-"""A Basic Flask app.
+"""
+Flask app
 """
 from flask import Flask, render_template
 
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route('/')
-def get_index() -> str:
-    """The home/index page.
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """
+    Handles / route
     """
     return render_template('0-index.html')
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(port="5000", host="0.0.0.0", debug=True)
