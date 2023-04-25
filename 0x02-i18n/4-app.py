1d81b497137d3d74c51ae5aefda0108175742cd8
Ermiyas <98635649+ermiyasmesfin@users.noreply.github.com>
2023-04-25 12:17:29 +0300

    last

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -19,7 +19,7 @@ app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
-@babel.localeselector
+# @babel.localeselector
 def get_locale() -> str:
     """Retrieves the locale for a web page.
     """
@@ -33,7 +33,7 @@ def get_locale() -> str:
             return query_table['locale']
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
-
+babel.init_app(app, locale_selector=get_locale)
 @app.route('/')
 def get_index() -> str:
     """The home/index page.
